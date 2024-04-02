34cd1dec6c8b0c6419360e3ff9ecfd726b4019f3
Marcus Bowman <miliarch.mb@gmail.com>
2024-04-01 20:54:18 -0700

    Finishing what I started I guess (that last commit was a halfie)

--- web_files/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from web_files.blueprints import site
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_prefixed_env()
-    app.register_blueprint(site, url_prefix='/')
-    return app
